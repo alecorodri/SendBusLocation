ed6600b559e794cab17d077d8022efbdceaa9a09
aleco <66926752+alecorodri@users.noreply.github.com>
2022-07-08 05:20:41 -0400

    add bus routes

--- lib/pages/travel_page.dart
+++ lib/pages/travel_page.dart
@@ -53,6 +53,86 @@ class _TravelPageState extends State<TravelPage> {
                     value: 7,
                     child: Text('Ruta 7'),
                   ),
+                  DropdownMenuItem(
+                    value: 8,
+                    child: Text('Ruta 8'),
+                  ),
+                  DropdownMenuItem(
+                    value: 9,
+                    child: Text('Ruta 9'),
+                  ),
+                  DropdownMenuItem(
+                    value: 10,
+                    child: Text('Ruta 10'),
+                  ),
+                  DropdownMenuItem(
+                    value: 11,
+                    child: Text('Ruta 11'),
+                  ),
+                  DropdownMenuItem(
+                    value: 12,
+                    child: Text('Ruta 12'),
+                  ),
+                  DropdownMenuItem(
+                    value: 13,
+                    child: Text('Ruta 13'),
+                  ),
+                  DropdownMenuItem(
+                    value: 14,
+                    child: Text('Ruta 14'),
+                  ),
+                  DropdownMenuItem(
+                    value: 15,
+                    child: Text('Ruta 15'),
+                  ),
+                  DropdownMenuItem(
+                    value: 16,
+                    child: Text('Ruta 16'),
+                  ),
+                  DropdownMenuItem(
+                    value: 17,
+                    child: Text('Ruta 17'),
+                  ),
+                  DropdownMenuItem(
+                    value: 18,
+                    child: Text('Ruta 18'),
+                  ),
+                  DropdownMenuItem(
+                    value: 19,
+                    child: Text('Ruta 19'),
+                  ),
+                  DropdownMenuItem(
+                    value: 20,
+                    child: Text('Ruta 20'),
+                  ),
+                  DropdownMenuItem(
+                    value: 21,
+                    child: Text('Ruta 21'),
+                  ),
+                  DropdownMenuItem(
+                    value: 22,
+                    child: Text('Ruta 22'),
+                  ),
+                  DropdownMenuItem(
+                    value: 23,
+                    child: Text('Ruta 23'),
+                  ),
+                  DropdownMenuItem(
+                    value: 24,
+                    child: Text('Ruta 24'),
+                  ),
+                  DropdownMenuItem(
+                    value: 25,
+                    child: Text('Ruta 25'),
+                  ),
+                  DropdownMenuItem(
+                    value: 26,
+                    child: Text('Ruta 26'),
+                  ),
+                  DropdownMenuItem(
+                    value: 27,
+                    child: Text('Ruta 27'),
+                  ),
                 ],
                 onChanged: (int? value) {
                   setState(() {
